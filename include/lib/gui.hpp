f51aa4923e0be9d5c02807ff15fea80a9f3d7888
Nikhil Dave <nikhilrdave2003@gmail.com>
2022-03-26 21:12:10 -0700

    Cleaned up documentation in gui.hpp

--- include/lib/gui.hpp
+++ include/lib/gui.hpp
@@ -6,8 +6,7 @@
  * than a class due to the nature of the LittleVGL C graphics library. I've
  * tried using classes, but they run into too many issues with LVGL functions
  * 
- * The GUI namespace contains functions that perform all of the necessary tasks for 
- * creating a LVGL object.
+ * The GUI namespace contains functions for creating more complex LVGL objects.
  */ 
 
 namespace GUI {
@@ -21,17 +20,11 @@ namespace GUI {
      * @param pressType: the button press type (Click, hold, long press, etc.)
      * @param function: the function to be called when the button is triggered
      * @param text: the label text on the button
-     * @param align: the LVGL alignment type, aligns relative to the parent object
-     * @param xCoord: the x coordinate offset from the given alignment
-     * @param yCoord: the y coordinate offset from the given alignment
-     * @param width: the width of the button 
-     * @param height: the height of the button  
      * 
      * @return A pointer to a new LVGL button created using the given specs   
      */ 
-    lv_obj_t * createButton(lv_obj_t * parent, lv_btn_action_t pressType, lv_action_t function,
-                            const char* text, lv_align_t align, lv_coord_t xCoord, 
-                            lv_coord_t yCoord, lv_coord_t width, lv_coord_t height);
+    lv_obj_t * createButton(lv_obj_t * parent, lv_btn_action_t pressType, 
+                            lv_action_t function, const char* text);
 
 
     /**
@@ -40,13 +33,10 @@ namespace GUI {
      * 
      * @param parent: the LVGL object the image should be created in
      * @param text: the text of the label
-     * @param align: the LVGL alignment type, aligns relative to the parent object
-     * @param xCoord: the x coordinate offset from the given alignment
-     * @param yCoord: the y coordinate offset from the given alignment
      * 
      * @return A pointer to a new LVGL label created using the given specs   
      */ 
-    lv_obj_t * createLabel(lv_obj_t * parent, const char* text, lv_align_t align, lv_coord_t xCoord, lv_coord_t yCoord);
+    lv_obj_t * createLabel(lv_obj_t * parent, const char* text);
 
     /**
      * A function to create an LVGL button matrix object. It packages all the LVGL functions to 
@@ -56,16 +46,9 @@ namespace GUI {
      * @param map: a pointer to a String array used to generate the matrix of
      *             buttons and their labels
      * @param function: the function to be called when a button in the matrix is clicked
-     * @param align: the LVGL alignment type, aligns relative to the parent object
-     * @param xCoord: the x coordinate offset from the given alignment
-     * @param yCoord: the y coordinate offset from the given alignment
-     * @param width: the width of the button matrix
-     * @param height: the height of the button matrix
      * 
      * @return A pointer to a new LVGL button matrix created using the given specs        
      */ 
-    lv_obj_t * createButtonMatrix(lv_obj_t * parent, const char* map[], lv_btnm_action_t function,
-                                  lv_align_t align, lv_coord_t xCoord, lv_coord_t yCoord,
-                                  lv_coord_t width, lv_coord_t height);
+    lv_obj_t * createButtonMatrix(lv_obj_t * parent, const char* map[], lv_btnm_action_t function);
 
 }
