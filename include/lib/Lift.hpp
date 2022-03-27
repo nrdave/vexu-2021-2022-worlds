bf8388c12aebeb8499ee592c2f7c0422e88da117
Nikhil Dave <nikhilrdave2003@gmail.com>
2022-03-26 20:53:50 -0700

    Expanded Lift class, added documentation Most config options now functions config options have default values opcontrol buttons now set in driver()

--- include/lib/Lift.hpp
+++ include/lib/Lift.hpp
@@ -1,29 +1,120 @@
-#include "lib/MotorGroup.hpp"
-
 #ifndef LIFT_HPP
 #define LIFT_HPP
 
+#include "lib/MotorGroup.hpp"
+
+/**
+ * The Lift class is used to abstract any sort of lift. It provides a simple interface to
+ * control an okapiLib MotorGroup.
+ * 
+ */ 
 class Lift {
     private:
+    // The okapiLib motor group that represents the motors on the lift
         MotorGroup motors;
+    /**
+     * The maximum speeds (in RPM) that the motors can travel at. 100 is the default
+     * as that is both a) a reasonable speed, and b) is the fastest motors with red
+     * gear cartridges can go.
+     */
+        int maxUpSpd = 100; 
+        int maxDownSpd = 100;
 
-        pros::controller_digital_e_t upBtn, downBtn;
+    /**
+     * The external gear ratio transferring power from the motor to the lift.
+     * Defaults to 1, which means that the rate of rotation for the motor and the
+     * mechanism driving the lift are the same.
+     */ 
+        double extGearRatio = 1.0;
 
-        int maxUpVelo, maxDownVelo;
-
+    /** 
+     * The minimum position of the motors for them to hold position,
+     * in degrees. The threshold is used
+     * to determine whether to have the motors hold their position or
+     * simply coast to a stop. This is useful for having the motors not hold
+     * position when close to the ground, where holding position is not important.
+     * This allows the motors to rest until their next use.
+     */ 
+        double holdThreshold = 0;
     public:
-        Lift(std::initializer_list<int> ports, std::initializer_list<bool> revs,
-           pros::controller_digital_e_t upButton, pros::controller_digital_e_t downButton,
-           pros::motor_encoder_units_e_t encoderUnits,
-           pros::motor_gearset_e_t gearing,
-           int maxUpRPM, int maxDownRPM);
+    /**
+     * The constructor for the Lift class
+     * 
+     * @param ports A list of the ports of the motors on the lift
+     * @param revs A list of booleans indicating which motors are reversed
+     */ 
+        Lift(std::initializer_list<int> ports, std::initializer_list<bool> revs);
 
-        void driver(pros::controller_id_e_t controller);
+    /*-------------------------
+     * Configuration functions
+     *-------------------------*/              
+    /**
+     * A function to change the internal gearing on the motors.
+     * If this is not called, the MotorGroup will assume a default
+     * value of green motor cartrigdes (max 200 RPM)
+     * 
+     * @param gearing The type of gear cartridge in the actual motors
+     */ 
+        void setGearing(pros::motor_gearset_e_t gearing);
 
+    /**
+     * A function to define the external gear ratio on the lift.
+     * Most lifts are not powered directly from a motor, instead
+     * being driven off of some sort of torque gear ratio, such as
+     * a 1:5 ratio, with a 12 tooth gear driving a 60 tooth gear.
+     * This function allows position-based measurements, such as
+     * the holdThreshold in stop() to account for the gear ratio.
+     * 
+     * @param ratio The gear ratio of the gear train transferring
+     *              power from the motor to the lift. It should be
+     *              input like so (teeth on driving gear) / (teeth on driven gear)
+     *              For example, for a 12 tooth to 60 tooth gear train, ratio is 12 / 60
+     */ 
+        void setExternalGearRatio(double ratio);
+
+    /**
+     * A function to change the maximum speeds, in RPM, of the lift motors.
+     * There are separate up and down speeds as, generally,
+     * having a lift go down slower is a good idea, as it prevents
+     * a rough impact on wherever the end of the lift rests.
+     * 
+     * @param maxUpSpeed The maximum speed of the motors while going up, in RPM
+     * @param maxDownSpeed The maximum speed of the motors while going down, in RPM
+     */ 
+        void setMaxSpeeds(int maxUpRPM, int maxDownRPM);
+
+    /**
+     * A function to change the threshold at which the motors
+     * hold position
+     */ 
+
+    /*-------------------
+     * Movement functions
+     *-------------------*/ 
+
+    /**
+     * A function used to control the lift while in opcontrol.
+     * It takes in the ID of the controller (generally this will be CONTROLLER_MASTER),
+     * as well as which buttons control the lift
+     * 
+     * @param controller The controller object representing the physical controller
+     * @param upButton The button on the controller used to move the lift up
+     * @param downButton The button on the controller used to move the lift down
+     */ 
+        void driver(pros::controller_id_e_t controller,
+           pros::controller_digital_e_t upButton, pros::controller_digital_e_t downButton);
+
+    /**
+     * A function to encapsulate moving the lift up 
+     */ 
         void up();
-
+    /**
+     * A function to encapsulate moving the lift down 
+     */ 
         void down();
-        
+    /**
+     * A function to encapsulate stopping the lift. 
+     */         
         void stop();
 };
 
--- src/lib/activeDev/Lift.cpp
+++ src/lib/activeDev/Lift.cpp
@@ -1,38 +1,52 @@
 #include "lib/Lift.hpp"
 
-Lift::Lift(std::initializer_list<int> ports, std::initializer_list<bool> revs,
-           pros::controller_digital_e_t upButton, pros::controller_digital_e_t downButton,
-           pros::motor_encoder_units_e_t encoderUnits,
-           pros::motor_gearset_e_t gearing,
-           int maxUpRPM, int maxDownRPM): 
-           motors(ports, revs)
-           {
-               motors.setEncoderUnits(encoderUnits);
-               motors.setGearing(gearing);
-               motors.setBrakeMode(pros::E_MOTOR_BRAKE_HOLD);
-
-               upBtn = upButton;
-               downBtn = downButton;
-               maxUpVelo = maxUpRPM;
-               maxDownVelo = maxDownRPM;
+Lift::Lift(std::initializer_list<int> ports, std::initializer_list<bool> revs): 
+           motors(ports, revs) {
+    // Encoders output their rotation in degrees (Other options don't really make sense)
+    motors.setEncoderUnits(pros::motor_encoder_units_e::E_MOTOR_ENCODER_DEGREES);
 }
 
-void Lift::driver(pros::controller_id_e_t controller) {
-    if (pros::c::controller_get_digital(controller, upBtn)) up();
-    else if (pros::c::controller_get_digital(controller, downBtn)) down();
+// Configuration Functions
+void Lift::setGearing(pros::motor_gearset_e_t gearing) {
+    motors.setGearing(gearing);
+}
+
+
+void Lift::setExternalGearRatio(double ratio) {
+    extGearRatio = ratio;
+}
+
+void Lift::setMaxSpeeds(int maxUpRPM, int maxDownRPM) {
+    maxUpSpd = maxUpRPM; 
+    maxDownSpd = maxDownRPM;
+}
+
+// Movement Functions
+void Lift::driver(pros::controller_id_e_t controller,
+                  pros::controller_digital_e_t upButton, 
+                  pros::controller_digital_e_t downButton) {
+    if (pros::c::controller_get_digital(controller, upButton)) up();
+    else if (pros::c::controller_get_digital(controller, downButton)) down();
     else stop();
 }
 
 void Lift::up() {
-    motors.moveVelocity(maxUpVelo);
+    motors.moveVelocity(maxUpSpd);
 }
 
 void Lift::down() {
-    motors.moveVelocity(maxDownVelo);
+    motors.moveVelocity(maxDownSpd);
 }
 
 void Lift::stop() {
-    if(motors.getPosition() <= 20) motors.setBrakeMode(pros::E_MOTOR_BRAKE_COAST);
+    /** If the motors current position is less than the holdThreshold * extGearRatio, set the brake mode to coast.
+     * Otherwise, have the motors hold position when they stop.
+     * 
+     * holdThreshold * extGearRatio is used because it represents the rotation of the motors themselves to get
+     * the lift to the holdThreshold. extGearRatio represents the number of rotations done by the lift in one
+     * full rotation of the motor.
+     */ 
+    if(motors.getPosition() <= (holdThreshold * extGearRatio) ) motors.setBrakeMode(pros::E_MOTOR_BRAKE_COAST);
     else motors.setBrakeMode(pros::E_MOTOR_BRAKE_HOLD);
     motors.moveVelocity(0);
 };
