e0a36da3efc253a1cb5f1875cb3e33e3dd58d3ff
Nikhil Dave <nikhilrdave2003@gmail.com>
2022-03-27 19:26:37 -0700

    Changed MotorGroup::getPosition - now returns avg pos of motors Also remove unnecessary braces from most MotorGroup functions

--- src/lib/activeDev/MotorGroup.cpp
+++ src/lib/activeDev/MotorGroup.cpp
@@ -5,10 +5,9 @@ MotorGroup::MotorGroup(std::initializer_list<int> ports, std::initializer_list<b
     /* Store the ports in the motorPorts vector*/
     motorPorts = ports;
     std::vector<bool> motorRevs = revs;
-    for(int i = 0; i < motorPorts.size(); ++i) {
+    for(int i = 0; i < motorPorts.size(); ++i) 
         // Set the reversed state of each motor to match the revs argument
         pros::c::motor_set_reversed(motorPorts[i], motorRevs[i]);
-    }
 }
 
 /**
@@ -18,56 +17,52 @@ MotorGroup::MotorGroup(std::initializer_list<int> ports, std::initializer_list<b
 
 /* Movement Functions */
 void MotorGroup::move(int voltage) {
-    for(int p : motorPorts) {
+    for(int p : motorPorts) 
         pros::c::motor_move(p, voltage);
-    }
 }
 
 void MotorGroup::moveAbsolute(double position, int velocity) {
-    for(int p : motorPorts) {
+    for(int p : motorPorts) 
         pros::c::motor_move_absolute(p, position, velocity);
-    }
 }
 
 void MotorGroup::moveRelative(double position, int velocity) {
-    for(int p : motorPorts) {
+    for(int p : motorPorts) 
         pros::c::motor_move_relative(p, position, velocity);
-    }
 }
 
 void MotorGroup::moveVelocity(int velocity) {
-    for(int p : motorPorts) {
+    for(int p : motorPorts) 
         pros::c::motor_move_velocity(p, velocity);
-    }
 }
 
 void MotorGroup::moveVoltage(int voltage) {
-    for(int p : motorPorts) {
+    for(int p : motorPorts) 
         pros::c::motor_move_voltage(p, voltage);
-    }
 }
 
 /* Configuration Functions */
 void MotorGroup::setEncoderUnits(pros::motor_encoder_units_e_t encoderUnits) {
-    for(int p : motorPorts) {
+    for(int p : motorPorts) 
         pros::c::motor_set_encoder_units(p, encoderUnits);
-    }
 }
 
 void MotorGroup::setBrakeMode(pros::motor_brake_mode_e_t brakeMode) {
-    for(int p : motorPorts) {
+    for(int p : motorPorts) 
         pros::c::motor_set_brake_mode(p, brakeMode);
-    }
 }
 
 void MotorGroup::setGearing(pros::motor_gearset_e_t gearing) {
-    for(int p : motorPorts) {
+    for(int p : motorPorts) 
         pros::c::motor_set_gearing(p, gearing);
-    }    
 }
 
 /* Telemetry Functions */
 
 double MotorGroup::getPosition() {
-    return pros::c::motor_get_position(motorPorts[0]);
+    double sum = 0;
+    for(int p : motorPorts){
+        sum += pros::c::motor_get_position(p)
+    }
+    return sum / motorPorts.size();
 }
