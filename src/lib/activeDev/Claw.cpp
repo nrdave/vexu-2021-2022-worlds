71cec3bfdd09d0dafb503cb91c4a480aabc4f3a2
Nikhil Dave <nikhilrdave2003@gmail.com>
2022-03-29 19:53:11 -0700

    Claw: Fixed driver functions, added timeout condition to openTo/closeTo openTo/closeTo is now blocking - allows moveRelative to reach target

--- src/lib/activeDev/Claw.cpp
+++ src/lib/activeDev/Claw.cpp
@@ -34,14 +34,14 @@ void Claw::driver(pros::controller_id_e_t controller,
                   pros::controller_digital_e_t digitalCloseButton,
                   pros::controller_digital_e_t digitalOpenButton) {
     if (pros::c::controller_get_digital(controller, digitalCloseButton))
-        closeTo(true);
+        closeTo();
     else if (pros::c::controller_get_digital(controller, digitalOpenButton))
-        openTo(false);
+        openTo();
 
     else
         // If neither digital button is pressed, call the simpler driver
         // function for analog control
-        driver(controller, openButton, closeButton);
+        driver(controller, closeButton, openButton);
 }
 
 void Claw::open() {
@@ -61,19 +61,35 @@ void Claw::close() {
 }
 
 void Claw::openTo(double degrees) {
+    int timeout = 0;
+
     // If the claw is opening, holding position is not important. So, it's
     // better to not have the motors hold position
     motors.setBrakeMode(pros::E_MOTOR_BRAKE_COAST);
     // The direction for the motors to rotate in order to open the claw is
     // assumed to be negative
     motors.moveRelative(-degrees, maxSpd);
+    while (!((motors.getPosition() > -degrees + 5) &&
+             (motors.getPosition() < -degrees - 5)) &&
+           timeout < 5) {
+        pros::delay(2);
+        ++timeout;
+    }
 }
 
 void Claw::closeTo(double degrees) {
+    int timeout = 0;
+
     // If the claw is closing, holding position is important in order to ensure
     // that the object being held is not let go of.
     motors.setBrakeMode(pros::E_MOTOR_BRAKE_HOLD);
     motors.moveRelative(degrees, maxSpd);
+    while (!((motors.getPosition() < degrees + 5) &&
+             (motors.getPosition() > degrees - 5)) &&
+           timeout < 5) {
+        pros::delay(2);
+        ++timeout;
+    }
 }
 
 // Overloaded openTo and closeTo functions that use digitalRotation
