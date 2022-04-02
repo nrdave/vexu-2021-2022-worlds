ddb62370b2a85cea86c262af214b601d3f187fa0
Nikhil Dave <nikhilrdave2003@gmail.com>
2022-04-02 14:50:09 -0700

    Switched back to motor claw control After testing, we decided pneumatics just didn't make sense for the claw

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -24,16 +24,8 @@ void opcontrol() {
             controller.getAnalog(okapi::ControllerAnalog::rightY));
         lift.driver(CONTROLLER_MASTER, DIGITAL_R1, DIGITAL_R2);
 
-        // claw.driver(CONTROLLER_MASTER, DIGITAL_L1, DIGITAL_L2, DIGITAL_A,
-        // DIGITAL_B);
-        if (pros::c::controller_get_digital(CONTROLLER_MASTER, DIGITAL_B))
-            ++extendCount;
-
-        if (extendCount >= 15) {
-            extended = !extended;
-            extendCount = 0;
-        }
-        clawPiston.set_value(extended);
+        claw.driver(CONTROLLER_MASTER, DIGITAL_L1, DIGITAL_L2, DIGITAL_A,
+                    DIGITAL_B);
 
         pros::delay(20);
     }
