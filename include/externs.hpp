b101d835738a2d5a6a38f0ff034f6bf229cddaad
Nikhil Dave <nikhilrdave2003@gmail.com>
2022-04-05 19:32:02 -0700

    Removed OkapiLib externs from externs.hpp

--- include/externs.hpp
+++ include/externs.hpp
@@ -2,15 +2,9 @@
 #include "lib/Lift.hpp"
 #include "lib/autonomous.hpp"
 #include "lib/gui.hpp"
-#include "okapi/api.hpp"
 
 #ifndef EXTERNS_HPP
 #define EXTERNS_HPP
-
-// The OkapiLib ChassisController and Controller, used solely for the drive
-extern std::shared_ptr<okapi::ChassisController> drive;
-
-extern okapi::Controller controller;
 
 // My Subsytem objects
 extern Lift lift;
