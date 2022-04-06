4d3197a8edade3a2a291c9f2c7f13a15f262eb57
Nikhil Dave <nikhilrdave2003@gmail.com>
2022-04-06 08:44:23 -0700

    externs.hpp: Moved #includes under header guard, removed unnecessary includes

--- include/externs.hpp
+++ include/externs.hpp
@@ -1,11 +1,9 @@
+#ifndef EXTERNS_HPP
+#define EXTERNS_HPP
+
 #include "lib/Claw.hpp"
 #include "lib/Lift.hpp"
 #include "lib/TankDrive.hpp"
-#include "lib/autonomous.hpp"
-#include "lib/gui.hpp"
-
-#ifndef EXTERNS_HPP
-#define EXTERNS_HPP
 
 // My Subsytem objects
 extern Lift lift;
