834819641b8d63f7a5caf0e8860055a368ac07bb
Nikhil Dave <nikhilrdave2003@gmail.com>
2022-04-07 19:20:31 -0700

    Moved function definitions for GUI in initialize to bottom of file I think it looks a bit cleaner

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -14,7 +14,7 @@ TankDrive drive({11, 12}, {4, 5}, {false, false}, {true, true});
  * to keep execution time for this mode under a few seconds.
  */
 
-// Declaring all LVGL objects and functions
+// Declaring all LVGL objects and function prototypes
 LV_IMG_DECLARE(background)
 
 lv_obj_t* scrMain;
@@ -25,6 +25,72 @@ lv_obj_t* navFromAuton;
 lv_obj_t* autonSelect;
 lv_obj_t* curAutonLbl;
 
+lv_res_t updateAutonID(lv_obj_t* btnm, const char* txt);
+void updateAutonLbl();
+lv_res_t goToAuton(lv_obj_t* btn);
+lv_res_t goToMain(lv_obj_t* btn);
+
+void initialize() {
+    // Configuring lift
+    lift.setExternalGearRatio(12.0 / 60.0);
+    lift.setGearing(MOTOR_GEARSET_18);
+    lift.setMaxSpeeds(150, 80);
+    lift.setHoldThreshold(20);
+
+    // Configuring drive
+    drive.setDimensions(3.25, 10);
+    drive.setGearing(pros::E_MOTOR_GEARSET_18);
+    drive.addADIEncoders('c', 'd', false, 'a', 'b', false);
+
+    // Configuring claw
+    // claw.setGearing(MOTOR_GEARSET_18);
+    // claw.setMaxSpeed(60);
+
+    scrMain = lv_obj_create(NULL, NULL);
+    scrAuton = lv_obj_create(NULL, NULL);
+
+    // GUI initialization
+    GUI::createImage(backgroundIMG, scrMain, &background);
+    lv_obj_align(backgroundIMG, NULL, LV_ALIGN_IN_TOP_LEFT, 0, 0);
+
+    GUI::createButton(navToAuton, scrMain, LV_BTN_ACTION_CLICK, goToAuton,
+                      "Auton Menu");
+    lv_obj_align(navToAuton, NULL, LV_ALIGN_IN_LEFT_MID, 10, 0);
+
+    GUI::createButton(navFromAuton, scrAuton, LV_BTN_ACTION_CLICK, goToMain,
+                      "Home Screen");
+    lv_obj_align(navFromAuton, NULL, LV_ALIGN_IN_LEFT_MID, 10, 0);
+
+    GUI::createButtonMatrix(autonSelect, scrAuton, Autonomous::buttonMatrixList,
+                            updateAutonID);
+    lv_obj_align(autonSelect, NULL, LV_ALIGN_IN_TOP_RIGHT, -10, 20);
+    lv_obj_set_size(autonSelect, 250, 200);
+
+    GUI::createLabel(curAutonLbl, scrAuton, "Auton");
+    lv_obj_align(curAutonLbl, NULL, LV_ALIGN_IN_TOP_LEFT, 10, 10);
+
+    lv_scr_load(scrMain);
+}
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System
+ * or the VEX Competition Switch, following either autonomous or opcontrol.
+ * When the robot is enabled, this task will exit.
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the
+ * Field Management System or the VEX Competition Switch. This is intended
+ * for competition-specific initialization routines, such as an autonomous
+ * selector on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+// GUI function definition
 void updateAutonLbl() {
     /**
      * Using autonID as a switch, the function determines
@@ -82,10 +148,9 @@ lv_res_t updateAutonID(lv_obj_t* btnm, const char* txt) {
 }
 
 /**
- * LVGL doesn't allow functions with parameters to be a callback function for a
- * button action, so I had to wrap a few functions, such as lv_scr_load(), which
- * loads a screen, in a format that the lv_btn_set_action() function would
- * accept
+ * LVGL doesn't allow functions with parameters to be a callback function
+ * for a button action, so I had to wrap lv_scr_load(), which loads a screen, in
+ * a format that the lv_btn_set_action() function would accept
  */
 lv_res_t goToAuton(lv_obj_t* btn) {
     lv_scr_load(scrAuton);
@@ -96,58 +161,3 @@ lv_res_t goToMain(lv_obj_t* btn) {
     lv_scr_load(scrMain);
     return LV_RES_OK;
 }
-
-void initialize() {
-    // Configuring lift
-    lift.setExternalGearRatio(12.0 / 60.0);
-    lift.setGearing(MOTOR_GEARSET_18);
-    lift.setMaxSpeeds(150, 80);
-    lift.setHoldThreshold(20);
-
-    // Configuring claw
-    // claw.setGearing(MOTOR_GEARSET_18);
-    // claw.setMaxSpeed(60);
-
-    scrMain = lv_obj_create(NULL, NULL);
-    scrAuton = lv_obj_create(NULL, NULL);
-
-    backgroundIMG = lv_img_create(scrMain, NULL);
-    lv_img_set_src(backgroundIMG, &background);
-    lv_obj_align(backgroundIMG, NULL, LV_ALIGN_IN_TOP_LEFT, 0, 0);
-
-    GUI::createButton(navToAuton, scrMain, LV_BTN_ACTION_CLICK, goToAuton,
-                      "Auton Menu");
-    lv_obj_align(navToAuton, NULL, LV_ALIGN_IN_LEFT_MID, 10, 0);
-
-    GUI::createButton(navFromAuton, scrAuton, LV_BTN_ACTION_CLICK, goToMain,
-                      "Home Screen");
-    lv_obj_align(navFromAuton, NULL, LV_ALIGN_IN_LEFT_MID, 10, 0);
-
-    GUI::createButtonMatrix(autonSelect, scrAuton, Autonomous::buttonMatrixList,
-                            updateAutonID);
-    lv_obj_align(autonSelect, NULL, LV_ALIGN_IN_TOP_RIGHT, -10, 20);
-    lv_obj_set_size(autonSelect, 250, 200);
-
-    GUI::createLabel(curAutonLbl, scrAuton, "Auton");
-    lv_obj_align(curAutonLbl, NULL, LV_ALIGN_IN_TOP_LEFT, 10, 10);
-
-    lv_scr_load(scrMain);
-}
-
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
-void disabled() {}
-
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
-void competition_initialize() {}
